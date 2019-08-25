64d8c8db31cbb58adaccfb14461f8dffa4fc598e
burov-web <burov.khv@gmail.com>
2019-08-25 15:17:51 +0200

    check duplicates in CreateWordService

--- app/Services/CreateWordService.php
+++ app/Services/CreateWordService.php
@@ -16,13 +16,37 @@ class CreateWordService
 	public static function store(CreateWordRequest $request)
     {
     	$validatedData = self::validate($request);
-        
+        //dd($validatedData);
+        $duplicateWords = self::getDuplicates($validatedData['word']);
+        if (!empty($duplicateWords)) {
+            dd($duplicateWords);
+        }
         return self::save($validatedData);
     }
 
     public static function validate(CreateWordRequest $request)
     {
     	return $request->validated();
+    }
+
+    public static function getDuplicates($morpheme)
+    {
+        $duplicateMorphemes = Words::getDuplicates($morpheme);
+        $duplicateWords = array();
+        foreach ($duplicateMorphemes as $duplicateMorpheme) {
+            array_push($duplicateWords, self::getFullWord($duplicateMorpheme));
+        }
+        return $duplicateWords;
+    }
+
+    public static function getFullWord(Words $morpheme)
+    {
+        $word = array();
+        $word['word'] = $morpheme->word;
+        $word['translate'] = $morpheme->translates;
+        $word['addParams'] = $morpheme->addParams;
+        $word['wordType'] = $morpheme->wordTypes;
+        return $word;
     }
 
     public static function save($params) {
