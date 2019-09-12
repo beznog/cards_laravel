e6a005de11d7e177ac7ffb539591b22654b1dffc
burov-web <burov.khv@gmail.com>
2019-09-12 22:34:57 +0200

    List words blade (not complete)

--- resources/views/list.blade.php
+++ resources/views/list.blade.php
@@ -7,10 +7,62 @@
         <ul>
             @foreach ($words as $word)
                 <li>
-                    {{ $word->morphemes['morpheme'] }} - 
+                    {{-- $word --}}
+                    @if ($word->wordTypes['word_type'] == "noun")
+                        {{ $word->addParams['article_type'] }}
+                    @endif
+                    {{ $word->morphemes['morpheme'] }}
+
+                    @if ($word->addParams['reflexive'])
+                        sich
+                    @endif
+                    @isset($word->addParams['preposition'])
+                        {{ $word->addParams['preposition'] }}
+                    @endisset
+
+                    @if ($word->wordTypes['word_type'] == "noun")
+                        (pl. {{ $word->addParams['plural'] }})
+                    @endif
+                        - 
                     @foreach ($word->translates as $translate)
                         {{ $translate->translate }}
+                        @if (!$loop->last)
+                            ,
+                        @endif
                     @endforeach
+                    
+                    @isset($word->addParams['prasens'])
+                        <br>
+                        {{ $word->addParams['prasens'] }}
+                    @endisset
+                    @isset($word->addParams['prateritum'])
+                        {{ $word->addParams['prateritum'] }}
+                    @endisset
+                    @isset($word->addParams['partizip'])
+                        @isset($word->addParams['modal_verb'])
+                            {{ $word->addParams['modal_verb'] }}
+                        @endisset
+                        {{ $word->addParams['partizip'] }}
+                    @endisset
+                    
+                    @isset($word->addParams['examples'])
+                        <br>
+                        examples: {{ $word->addParams['examples'] }}
+                    @endisset
+                    
+                    @isset($word->addParams['importance'])
+                        <br>
+                        importance: {{ $word->addParams['importance'] }},
+                    @endisset
+                    @isset($word->addParams['complexity'])
+                        complexity: {{ $word->addParams['complexity'] }},
+                    @endisset   
+                    @isset($word->addParams['knowledge'])
+                        knowledge: {{ $word->addParams['knowledge'] }}
+                    @endisset
+
+                    <br>
+                    <br>            
                 </li>
             @endforeach
         </ul>
